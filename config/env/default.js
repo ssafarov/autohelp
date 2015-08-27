4685c66a478b727f35fad6427ffcffc37b985295
Safarov, Sergey <s.safarov@itransition.com>
2015-08-27 10:45:03 +0300

    Editing configs

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'autoHelp',
+    description: 'Global application for Human Safety',
+    keywords: 'human, safety',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
@@ -13,10 +13,10 @@ module.exports = {
   // session expiration is set by default to 24 hours
   sessionExpiration: 24 * (60 * 60 * 1000),
   // sessionSecret should be changed for security measures and concerns
-  sessionSecret: 'MEAN',
+  sessionSecret: 'autoHelp-pcxbm5H3GczQS9Gxhp',
   // sessionKey is set to the generic sessionId key used by PHP applications
   // for obsecurity reasons
-  sessionKey: 'sessionId',
+  sessionKey: 'pcxbm5H3GczQS9Gxhp',
   sessionCollection: 'sessions',
   logo: 'modules/core/client/img/brand/logo.png',
   favicon: 'modules/core/client/img/brand/favicon.ico'
