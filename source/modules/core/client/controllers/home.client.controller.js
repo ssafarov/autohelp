64f8bc25bd423e7d4d8498b6fd7635b0e8d7de63
Safarov, Sergey <s.safarov@itransition.com>
2015-12-04 18:21:45 +0300

    Works with bluring

--- source/modules/core/client/controllers/home.client.controller.js
+++ source/modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,9 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', '$state', 'Authentication',
+  function ($scope, $state, Authentication) {
     // This provides Authentication context.
+    $state.needBlur = false;
     $scope.authentication = Authentication;
   }
 ]);
